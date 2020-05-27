f47fa21f1ef2585918517a1315b80dc138c3dd56
Khali851999 <akshatjindal215@gmail.com>
2020-05-28 02:53:24 +0530

    Trees

---
+++ Tree/a.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<vector<int>> g;
+bool dfs(int src, vector<int> &vis, int clr)
+{
+    if (vis[src] != -1 && vis[src] != clr)
+        return 0;
+    vis[src] = clr;
+    int ans = 1;
+    for (auto x : g[src])
+    {
+        int clr1 = (clr + 1) % 2;
+        if (vis[x] == -1)
+            ans &= dfs(x, vis, clr1);
+        if (vis[x] != clr1)
+            return 0;
+    }
+    return ans;
+}
+bool isBipartite(int n, vector<vector<int>> &edges)
+{
+    g.resize(n, vector<int>());
+    for (auto x : edges)
+    {
+        int u = x[0]-1;
+        int v = x[1]-1;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+    vector<int> vis(n, -1);
+    return dfs(0, vis, 0);
+}
+int main()
+{
+    vector<vector<int>> edges = {{1, 2}, {2, 3}, {3, 4}, {4, 5}, {2, 4}};
+    cout << isBipartite(5, edges);
+}
