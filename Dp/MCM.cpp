b6ff69589dcd2f0f7278482fb7aad8212ceefd42
Khali851999 <akshatjindal215@gmail.com>
2020-07-15 18:17:30 +0530

    s

--- Dp/MCM.cpp
+++ Dp/MCM.cpp
@@ -52,8 +52,8 @@ int MCM_BU(vector<int> &arr, int si, int ei, vector<vector<int>> &dp)
             int ans = INT_MAX;
             for (int cut = si + 1; cut < ei; cut++)
             {
-                int left = MCM_TD(arr, si, cut, dp);
-                int right = MCM_TD(arr, cut, ei, dp);
+                int left = dp[si][cut];
+                int right = dp[cut][ei];
                 int cost = left + arr[si] * arr[ei] * arr[cut] + right;
                 ans = min(ans, cost);
             }
@@ -64,9 +64,51 @@ int MCM_BU(vector<int> &arr, int si, int ei, vector<vector<int>> &dp)
 
     return dp[0][n - 1];
 }
+int MCM_Brackets(vector<int> &arr, int si, int ei, vector<vector<int>> &dp)
+{
+    int n = arr.size();
+    vector<vector<string>> sdp(n, vector<string>(n, ""));
 
+    for (int gap = 1; gap < n; gap++)
+    {
+        for (si = 0, ei = gap; ei < n; si++, ei++)
+        {
+            if (si + 1 == ei)
+            {
+                dp[si][ei] = 0;
+                sdp[si][ei] = si + 'A';
+                continue;
+            }
+            int ans = INT_MAX;
+            for (int cut = si + 1; cut < ei; cut++)
+            {
+                int left = dp[si][cut];
+                int right = dp[cut][ei];
+                int cost = left + arr[si] * arr[ei] * arr[cut] + right;
+
+                if (cost < ans)
+                {
+                    ans = cost;
+                    sdp[si][ei] = "(" + sdp[si][cut] + sdp[cut][ei] + ")";
+                }
+            }
+
+            dp[si][ei] = ans;
+        }
+    }
+
+    // for (auto x : sdp)
+    // {
+    //     for (auto y : x)
+    //         cout << y << " ";
+    //     cout << endl;
+    // }
+
+    cout << sdp[0][n - 1];
+    return dp[0][n - 1];
+}
 //==================================OPTIMAL BST===================================
-int OptimalBST_TD(vector<int> &key, vector<int> &freq, int si, int ei, vector<vector<int>> &dp, vector<int>& prefixSum)
+int OptimalBST_TD(vector<int> &key, vector<int> &freq, int si, int ei, vector<vector<int>> &dp, vector<int> &prefixSum)
 {
 
     if (dp[si][ei] != -1)
@@ -88,8 +130,8 @@ int OptimalBST_TD(vector<int> &key, vector<int> &freq, int si, int ei, vector<ve
 }
 void OptimalBST()
 {
-    vector<int> keys={};
-    vector<int> freq={};
+    vector<int> keys = {};
+    vector<int> freq = {};
     sort(keys.begin(), keys.end());
 }
 
@@ -104,6 +146,7 @@ void MCM()
     display2D(dp);
     MCM_BU(arr, 0, n - 1, dp);
     display2D(dp);
+    MCM_Brackets(arr, 0, n, dp);
 }
 
 //==============================================================================================================
